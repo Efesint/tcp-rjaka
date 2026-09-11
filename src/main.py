fe1ed160b898d3bdaa40edcc527c5fa7e27906d3
efe <brajanmozer53@gmail.com>
2026-09-12 02:37:23 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,37 @@
 import socket
+import os
+import sys
+
+import time
+
+os.system("clear")
+art = r"""
+ ______________          ___     _____   __ _____ 
+/_  __/ ___/ _ \  ____  / _ \__ / / _ | / //_/ _ |
+ / / / /__/ ___/ /___/ / , _/ // / __ |/ ,< / __ |
+/_/  \___/_/          /_/|_|\___/_/ |_/_/|_/_/ |_|
+
+"""
+
+def check_con():
+    try: 
+        socket.create_connection(("8.8.8.8", 53), timeout=3)
+        return True
+    except OSError:
+        return False
+
+if check_con():
+   print("\033[32m")
+   print("[+] Internet connection is stable.")
+else:
+   time.sleep(1)
+   print("\033[31m")
+   print("[-] No internet connection.")
+   print("[-] Exiting the program")
+   sys.exit(1)
+
+print("\033[90m")
+print(art)
+
+
+
