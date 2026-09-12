76b37c961a212b34c14b82b081e7b03fdb4b659c
efe <brajanmozer53@gmail.com>
2026-09-13 01:43:16 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 import socket
 import os
 import sys
+import subprocess
 
 import time
 
-os.system("clear")
 art = r"""
  ______________          ___     _____   __ _____ 
 /_  __/ ___/ _ \  ____  / _ \__ / / _ | / //_/ _ |
  / / / /__/ ___/ /___/ / , _/ // / __ |/ ,< / __ |
 /_/  \___/_/          /_/|_|\___/_/ |_/_/|_/_/ |_|
-
 """
 
 def check_con():
@@ -19,6 +18,8 @@ def check_con():
         return True
     except OSError:
         return False
+
+os.system("clear")     
 
 if check_con():
    print("\033[32m")
@@ -33,5 +34,23 @@ else:
 print("\033[90m")
 print(art)
 
+run = True
+print("What should be done?")
+print("\u001b[31m")
 
+print("\n[1] Scan tcp ports")
+print("[0] exit\n")
 
+while run == True:
+    try:
+        choise = int(input())
+    except ValueError:
+        print("Enter the number")
+        continue
+    if choise == 1:
+        print("\nWrite the ip address to scan\n")
+        ip = input()
+        print("\n[x]", ip) 
+    elif choise == 0:
+         print("exiting the programm..")
+         run = False
